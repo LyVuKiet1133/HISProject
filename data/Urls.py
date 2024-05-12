9b63c0e9aae2a176b098f523f27498323f99bfe8
VuKiet <k0327512353@gmail.com>
2024-05-12 18:37:32 +0700

    api create patients

---
+++ data/Urls.py
@@ -0,0 +1,6 @@
+class URLS():
+    url_1081 = 'http://192.168.110.8:1081'
+    url_1083 = 'http://192.168.110.8:1083'
+    API_LOGIN = f'{url_1081}/Users/Login/ducpn?pwd=202cb962ac59075b964b07152d234b70'
+    API_CREATE_PATIENT = f'{url_1083}/Patients'
+
